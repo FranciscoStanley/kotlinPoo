873a6e808c72bcd6b23e8e1b7f1803b1327c434b
Francisco Stanley Rodrigues Albuquerque <38919705+FranciscoStanley@users.noreply.github.com>
2021-07-14 01:04:10 -0300

    Created, one private set for var cpf, and created var identidade.

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -2,10 +2,21 @@ class Pessoa {
 
     var nome:String = "Francisco Stanley"
     var idade:Int = 25
+    var identidade:String = "1.123.123"
     var cpf:String = "123.123.123-11"
+        private set
     var nTelefone:String = "(61)91234-1234"
-    var endereco:String = "Rua programando em Kotlin casa 404, bairro not found!"
     var profissao:String = "Software Developer"
+
+
+
+    inner class endereco {
+
+        var rua:String = "Rua programando em Kotlin"
+        var nCasa:Int = 404
+        var bairro:String = "Not Found"
+
+    }
 
 }
 
@@ -15,9 +26,14 @@ fun main(){
 
     println("Nome: ${franciscoStanley.nome}.")
     println("Idade: ${franciscoStanley.idade} Anos.")
+    println("Identidade: ${franciscoStanley.identidade}.")
     println("CPF: ${franciscoStanley.cpf}.")
     println("N°Telefone: ${franciscoStanley.nTelefone}.")
-    println("Endereço: ${franciscoStanley.endereco}.")
+
+    println("Endereço: ${franciscoStanley.endereco().rua}, " +
+            "casa ${franciscoStanley.endereco().nCasa}, " +
+            "bairro ${franciscoStanley.endereco().bairro}.")
+
     println("Profissão: ${franciscoStanley.profissao}.")
 
 }
