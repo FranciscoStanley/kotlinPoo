aad8bd8b2d1818b90c7a8f24e6b731940c255787
Francisco Stanley Rodrigues Albuquerque <38919705+FranciscoStanley@users.noreply.github.com>
2021-07-16 23:41:54 -0300

    Created acount class

---
+++ src/main/kotlin/one/digitalinovattion/Conta.kt
@@ -0,0 +1,25 @@
+package one.digitalinovattion
+
+import java.math.BigDecimal
+
+//Criando construtor primario
+class Conta(
+
+    val agencia:String,
+    val numero:String,
+    val saldo:BigDecimal
+
+) {
+
+    fun deposito(valor:BigDecimal){
+
+
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+
+
+    }
+}
