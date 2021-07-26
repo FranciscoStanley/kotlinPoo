87db331bb784b7759b4be2245ef7ed5e7960d06d
Francisco Stanley Rodrigues Albuquerque <38919705+FranciscoStanley@users.noreply.github.com>
2021-07-26 16:37:57 -0300

    Testes

--- src/main/kotlin/one/digitalinovattion/Pessoa.kt
+++ src/main/kotlin/one/digitalinovattion/Pessoa.kt
@@ -1,55 +1,6 @@
 package one.digitalinovattion
 
-class Pessoa {
-
-    var nome:String = "Francisco Stanley"
-    var idade:Int = 25
-    var identidade:String = "1.123.123"
-    var cpf:String = "123.123.123-11"
-        private set
-    var nTelefone:String = "(61)91234-1234"
-    var profissao:String = "Software Developer"
-
-
-    //Criando um inner class para retorna endereço
-    inner class endereco {
-
-        var rua:String = "Rua programando em Kotlin"
-        var nCasa:Int = 404
-        var bairro:String = "Not Found"
-
-    }
-
-    //Criando um construtor e função para retorna nome e cpf
-    constructor()
-
-    fun pessoaInfo() = "Nome: $nome | CPF: $cpf"
-
-
-
-}
-
-fun main(){
-
-    //Intanciando objeto pessoa
-
-    val franciscoStanley = Pessoa()
-
-//Imprimindo as informações da classe pessoa
-
-    println("Nome: ${franciscoStanley.nome}.")
-    println("Idade: ${franciscoStanley.idade} Anos.")
-    println("Identidade: ${franciscoStanley.identidade}.")
-    println("CPF: ${franciscoStanley.cpf}.")
-    println("N°Telefone: ${franciscoStanley.nTelefone}.")
-
-    println("Endereço: ${franciscoStanley.endereco().rua}, " +
-            "casa ${franciscoStanley.endereco().nCasa}, " +
-            "bairro ${franciscoStanley.endereco().bairro}.")
-
-    println("Profissão: ${franciscoStanley.profissao}.")
-
-    //Imprimindo o construtor da função pessoaInfo
-    println(franciscoStanley.pessoaInfo())
-
-}
+open class Pessoa(
+    open val nome: String,
+    open val cpf: String
+)
---
+++ src/main/kotlin/one/digitalinovattion/testes/TestPessoa.kt
@@ -0,0 +1,11 @@
+package one.digitalinovattion.testes;
+
+import one.digitalinovattion.Pessoa
+
+fun main(){
+
+    val francisco = Pessoa(nome = "Francisco Stanley", cpf = "123.123.123-21")
+    println(francisco.nome)
+    println(francisco.cpf)
+
+}
